343a8bbd03d034025af1e9fb5695afde464332cf
makakochka <makakochka@gmail.com>
2024-12-10 18:18:41 +0900

    second commit

---
+++ edit_user.py
@@ -0,0 +1,28 @@
+from db_config import Message
+from name_criterion import name_criterion
+from age_criterion import age_criterion
+
+
+def edit_user(find_users):
+    entered_name = name_criterion(input("User name > "))
+    if entered_name in find_users:
+        for msg in Message.select():
+            if msg.user == entered_name:
+                try:
+                    new_user_name = input("New user name > ")
+                    msg.user = new_user_name
+                    new_age_input = input(f"New user age({msg.age}) > ")
+                    new_age = int(new_age_input)  # Convert the input to an integer
+                    new_age = age_criterion(new_age)
+                    if new_age is not None:
+                        msg.age = new_age
+                        msg.save()
+                        print(
+                            f"{entered_name} whose age was {msg.age} has been updated to "
+                            f"{new_user_name} whose age is {new_age}"
+                        )
+                except ValueError:
+                    print("Please enter a valid number for the age.")
+                break
+    else:
+        print(f"Sorry, {entered_name} is invalid")
---
+++ name_criterion.py
@@ -0,0 +1,5 @@
+def name_criterion(name):
+    if len(name) > 20:
+        print("User name should be within 20 characters")
+        return name
+    return name
